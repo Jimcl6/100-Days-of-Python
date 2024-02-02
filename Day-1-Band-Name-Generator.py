9edffc23975bf3ce3d867ca1e10b01b3e1e2d1f1
Jimcl6 <jedllorente@gmail.com>
2024-02-02 20:40:09 +0800

    added projects Day 1 and Day 2

---
+++ Day-1-Band-Name-Generator.py
@@ -0,0 +1,4 @@
+print(' Welcome to the pet Name Generator')
+gender_title = input('Please select a Gender Title: Mr. Ms. Mrs. and Miss.\n')
+chosen_name = input('Please enter the name of your favorite desert.\n')
+print(f"Your pet's name is: {gender_title} {chosen_name}.")
